6262ce65cdb0bab3dffda990aa03ff8c77b14edb
King Pra <kingi.pra@gmail.com>
2019-02-20 19:51:39 -0500

    fixed cors

--- server.js
+++ server.js
@@ -1,28 +1,67 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
 const app = express();
 
 // Bodyparser Middleware
-//app.use(bodyParser.json());
-
+app.use(bodyParser.json());
+app.use(
+  cors({
+    credentials: true
+  })
+);
 // express update since the video was made:
-app.use(express.json());
+//app.use(express.json(), cors());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
 
 //connect to Mongo
 mongoose
-  .connect(db)
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("mongoDB Connected..."))
-  .catch(err => console.log(err));
+  .catch(err => console.log("mongo error... maybe : ", err));
 
 //Use Routes
 app.use("/api/items", items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+// app.use(function(req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 const port = process.env.PORT || 5000;
 
