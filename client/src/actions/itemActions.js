329ca8468e6a6254ab58b310365e22c6ca64672c
King Pra <kingi.pra@gmail.com>
2019-02-21 19:04:13 -0500

    change action routes

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -13,7 +13,7 @@ import axios from "axios";
 
 export const getItems = () => dispatch => {
   dispatch(setItemsLoading());
-  axios.get("http://localhost:5000/api/items").then(res =>
+  axios.get("/api/items").then(res =>
     dispatch({
       type: GET_ITEMS,
       payload: res.data
@@ -22,7 +22,7 @@ export const getItems = () => dispatch => {
 };
 
 export const addItem = item => dispatch => {
-  axios.post("http://localhost:5000/api/items", item).then(res =>
+  axios.post("/api/items", item).then(res =>
     dispatch({
       type: ADD_ITEM,
       payload: res.data
@@ -31,7 +31,7 @@ export const addItem = item => dispatch => {
 };
 
 export const deleteItem = id => dispatch => {
-  axios.delete(`http://localhost:5000/api/items/${id}`).then(res =>
+  axios.delete(`/items/${id}`).then(res =>
     dispatch({
       type: DELETE_ITEM,
       payload: id
